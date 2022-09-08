7c01337e0b47e311169ae6912605a6eaefe90144
Matthew Schranz <schranz.m@gmail.com>
2022-09-07 20:35:28 -0400

    More basic finished

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
 const http = require("http");
+const fs = require("fs");
+const writeFileSyncRecursive = require("./utils/writeFileSyncRecursive");
 
 function rqListener(req, res) {
-  console.log(req.url, req.method, req.headers);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write(`
+      <html>
+        <head><title>My First Page</tile></head></title>
+        <body>
+          <form action="/message" method="POST">
+            <input type="text" name="message">
+            <button type="submit">Submit</button>
+          </form>
+        </body>
+      </html>
+    `);
+    return res.end(); 
+  }
+
+  if (url === "/message" && method === "POST") {
+    const body = [];
+
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      writeFileSyncRecursive("./files/message.txt", `${parsedBody.split("=")[1]}\n`);
+    });
+    req.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
 
   res.setHeader("Content-Type", "text/html");
   res.write(`
@@ -11,6 +45,7 @@ function rqListener(req, res) {
     </html>
   `);
   res.end();
+
 }
 
 const server = http.createServer(rqListener);
