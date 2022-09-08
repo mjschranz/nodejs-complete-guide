8d1f0b74e4444a442ebd625755f026f7f7c7a925
Matthew Schranz <schranz.m@gmail.com>
2022-09-07 20:51:19 -0400

    Some additional work for 3-36

--- app.js
+++ app.js
@@ -1,53 +1,40 @@
-const http = require("http");
-const fs = require("fs");
-const writeFileSyncRecursive = require("./utils/writeFileSyncRecursive");
+const http = require('http');
+const fs = require('fs');
 
-function rqListener(req, res) {
+const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
-
-  if (url === "/") {
-    res.setHeader("Content-Type", "text/html");
-    res.write(`
-      <html>
-        <head><title>My First Page</tile></head></title>
-        <body>
-          <form action="/message" method="POST">
-            <input type="text" name="message">
-            <button type="submit">Submit</button>
-          </form>
-        </body>
-      </html>
-    `);
-    return res.end(); 
-  }
-
-  if (url === "/message" && method === "POST") {
-    const body = [];
-
-    req.on("data", (chunk) => {
-      body.push(chunk);
-    });
-    req.on("end", () => {
-      const parsedBody = Buffer.concat(body).toString();
-      writeFileSyncRecursive("./files/message.txt", `${parsedBody.split("=")[1]}\n`);
-    });
-    req.statusCode = 302;
-    res.setHeader("Location", "/");
+  if (url === '/') {
+    res.write('<html>');
+    res.write('<head><title>Enter Message</title><head>');
+    res.write(
+      '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
+    );
+    res.write('</html>');
     return res.end();
   }
-
-  res.setHeader("Content-Type", "text/html");
-  res.write(`
-    <html>
-      <head><title>My First Page</tile></head></title>
-      <body><h1>Hello from my Node.js Server!</h1></body>
-    </html>
-  `);
+  if (url === '/message' && method === 'POST') {
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
+    });
+  }
+  res.setHeader('Content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
+  res.write('</html>');
   res.end();
+});
 
-}
-
-const server = http.createServer(rqListener);
-
-server.listen(process.env.NODEJS_PORT || 3000);
+server.listen(3000);
--- utils/writeFileSyncRecursive.js
+++ utils/writeFileSyncRecursive.js
@@ -2,18 +2,18 @@ const path = require('path');
 const fs = require('fs');
 
 function writeFileSyncRecursive(filename, content, charset) {
-  const folders = filename.split(path.sep).slice(0, -1)
+  const folders = filename.split(path.sep).slice(0, -1);
   if (folders.length) {
     // create folder path if it doesn't exist
     folders.reduce((last, folder) => {
-      const folderPath = last ? last + path.sep + folder : folder
+      const folderPath = last ? last + path.sep + folder : folder;
       if (!fs.existsSync(folderPath)) {
-        fs.mkdirSync(folderPath)
+        fs.mkdirSync(folderPath);
       }
-      return folderPath
-    })
+      return folderPath;
+    });
   }
-  fs.writeFileSync(filename, content, charset)
+  fs.writeFileSync(filename, content, charset);
 }
 
 module.exports = writeFileSyncRecursive;
